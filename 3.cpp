84de815650befe4e2a1eaad6b3f7334b2b66361c
yuki <yukitake2@icloud.com>
2023-05-11 19:50:28 +0900

    ファイルを更新

---
+++ 3.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+//#include <atcoder/all>
+//using namespace atcoder;
+//using mint = modint998244353;
+// using mint;  /*このときmint::set_mod(mod)のようにしてmodを底にする*/
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define repi(i,a,b) for(int i = a; i <= (int)(b); i++)
+#define rng(i,a,b) for(int i = a; i < (int)(b); i++)
+#define rrng(i,a,b) for(int i = a; i >= (int)(b); i--)
+#define pb push_back
+#define eb emplace_back
+#define pob pop_back
+#define si(a) (int)a.size()
+#define all(a) a.begin(),a.end()
+#define rall(a) a.rbegin(),a.rend()
+#define ret(x) { cout<<(x)<<endl;}
+typedef long long ll;
+using namespace std;
+using P = pair<ll,ll>;
+const ll LINF = 1001002003004005006ll;
+const int INF = 1001001001;
+ll mul(ll a, ll b) { if (a == 0) return 0; if (LINF / a < b) return LINF; return min(LINF, a * b); }
+ll mod(ll x, ll y){return (x % y + y) % y;}
+char itoc(int x){return x + '0';}
+int ctoi(char c){return c - '0';}
+
+vector<pair<long long, long long> > prime_factorize(long long N) {
+    vector<pair<long long, long long> > res;
+    for (long long a = 2; a * a <= N; ++a) {
+        if (N % a != 0) continue;
+        long long ex = 0; // 指数
+
+        // 割れる限り割り続ける
+        while (N % a == 0) {
+            ++ex;
+            N /= a;
+        }
+
+        // その結果を push
+        res.push_back({a, ex});
+    }
+
+    // 最後に残った数について
+    if (N != 1) res.push_back({N, 1});
+    return res;
+}
+
+int main(){
+
+    ll n = 600851475143;
+    vector<P> a = prime_factorize(n);
+    sort(rall(a));
+
+    cout << a[0].first << endl;
+
+    return 0;
+}
