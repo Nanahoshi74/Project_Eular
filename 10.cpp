a906c4d0222e9f1eb4769eba8d3ca0f509afc891
yuki <yukitake2@icloud.com>
2023-05-17 20:22:45 +0900

    10.cpp を追加

---
+++ 10.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+#include <numeric>
+//#include <atcoder/all>
+//using namespace atcoder;
+//using mint = modint998244353;
+// using mint;  /*このときmint::set_mod(mod)のようにしてmodを底にする*/
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define repi(i,a,b) for(int i = a; i <= (int)(b); i++)
+#define rng(i,a,b) for(int i = a; i < (int)(b); i++)
+#define rrng(i,a,b) for(int i = a; i >= (int)(b); i--)
+#define pb push_back
+#define eb emplace_back
+#define pob pop_back
+#define si(a) (int)a.size()
+#define all(a) a.begin(),a.end()
+#define rall(a) a.rbegin(),a.rend()
+#define ret(x) { cout<<(x)<<endl;}
+typedef long long ll;
+using namespace std;
+using P = pair<ll,ll>;
+const ll LINF = 1001002003004005006ll;
+const int INF = 1001001001;
+ll mul(ll a, ll b) { if (a == 0) return 0; if (LINF / a < b) return LINF; return min(LINF, a * b); }
+ll mod(ll x, ll y){return (x % y + y) % y;}
+char itoc(int x){return x + '0';}
+int ctoi(char c){return c - '0';}
+
+vector<ll> Eratosthenes( const ll N )//********************素数列挙******************
+{
+    vector<bool> is_prime( N + 1 );
+    for( int i = 0; i <= N; i++ )
+    {
+        is_prime[ i ] = true;
+    }
+    vector<ll> P;
+    for( int i = 2; i <= N; i++ )
+    {
+        if( is_prime[ i ] )
+        {
+            for( int j = 2 * i; j <= N; j += i )
+            {
+                is_prime[ j ] = false;
+            }
+            P.emplace_back( i );
+        }
+    }
+    return P;
+}
+
+int main(){
+
+    ll n = 2e6;
+    vector<ll> a = Eratosthenes(n);
+
+    ll sum = 0;
+    rep(i,si(a)){
+    sum += a[i];
+    }
+
+    cout << sum << endl;
+
+    return 0;
+}
