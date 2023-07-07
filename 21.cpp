810388e7aa2837895113bf17ecb211f4d3ede91b
yuki <yukitake2@icloud.com>
2023-07-07 15:28:41 +0900

    21.cpp を追加

---
+++ 21.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+//#include <atcoder/all>
+//using namespace atcoder;
+//using mint = modint998244353;
+// using mint;  /*このときmint::set_mod(mod)のようにしてmodを底にする*/
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define repi(i,a,b) for(int i = a; i <= (int)(b); i++)
+#define rng(i,a,b) for(int i = a; i < (int)(b); i++)
+#define rrng(i,a,b) for(int i = a; i >= (int)(b); i--)
+#define pb push_back
+#define eb emplace_back
+#define pob pop_back
+#define si(a) (int)a.size()
+#define all(a) a.begin(),a.end()
+#define rall(a) a.rbegin(),a.rend()
+#define ret(x) { cout<<(x)<<endl;}
+typedef long long ll;
+using namespace std;
+using P = pair<ll,ll>;
+const ll LINF = 1001002003004005006ll;
+const int INF = 1001001001;
+ll mul(ll a, ll b) { if (a == 0) return 0; if (LINF / a < b) return LINF; return min(LINF, a * b); }
+ll mod(ll x, ll y){return (x % y + y) % y;}
+char itoc(int x){return x + '0';}
+int ctoi(char c){return c - '0';}
+
+vector<long long> divisor(long long n) {
+    vector<long long> ret;
+    for (long long i = 1; i * i <= n; i++) {
+        if (n % i == 0) {
+            ret.push_back(i);
+            if (i * i != n) ret.push_back(n / i);
+        }
+    }
+    sort(ret.begin(), ret.end()); // 昇順に並べる
+    return ret;
+}
+
+int main(){
+
+    map<ll,ll> mp;
+    ll ans = 0;
+    rng(i,1,10001){
+        vector<ll> p = divisor(i);
+        ll sum = 0;
+        rep(j,si(p)-1){
+            sum += p[j];
+        }
+        mp[i] = sum;
+        if(mp[sum] == i){
+            if(i != sum){
+                ans += (i + sum);
+            }
+        }
+    }
+
+    cout << ans << endl;
+
+
+    return 0;
+}
