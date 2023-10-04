0a037e945c43532354def9b795c3d411023796e7
yuki <yukitake2@icloud.com>
2023-10-04 12:23:33 +0900

    sample.cpp を更新

--- sample.cpp
+++ sample.cpp
@@ -1,7 +1,8 @@
 #include <bits/stdc++.h>
-//#include <atcoder/all>
-//using namespace atcoder;
-//using mint = modint998244353;
+#include <atcoder/all>
+using namespace atcoder;
+using mint = modint998244353;
+//using mint = modint1000000007;
 // using mint;  /*このときmint::set_mod(mod)のようにしてmodを底にする*/
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
 #define repi(i,a,b) for(int i = a; i <= (int)(b); i++)
@@ -23,6 +24,12 @@ ll mul(ll a, ll b) { if (a == 0) return 0; if (LINF / a < b) return LINF; return
 ll mod(ll x, ll y){return (x % y + y) % y;}
 char itoc(int x){return x + '0';}
 int ctoi(char c){return c - '0';}
+void chmin(ll& x,ll y){x = min(x,y);}
+void chmax(ll& x,ll y){x = max(x,y);}
+ll getnum(ll x, ll y, ll H, ll W) { (void) H; return (x * W + y);}
+template<typename T>
+void print(vector<T> &p){rep(i,si(p)) cout << p[i] << " "; cout << endl;}
+
 
 int main(){
 
