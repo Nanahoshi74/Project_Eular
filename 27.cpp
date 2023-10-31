39c5756ccfa75af9f0138f4155953839e3463040
yuki <yukitake2@icloud.com>
2023-10-31 19:23:35 +0900

    27.cpp を追加

---
+++ 27.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace atcoder;
+using mint = modint998244353;
+//using mint = modint1000000007;
+// using mint;  /*このときmint::set_mod(mod)のようにしてmodを底にする*/
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define repi(i,a,b) for(int i = a; i <= (int)(b); i++)
+#define rng(i,a,b) for(int i = a; i < (int)(b); i++)
+#define rrng(i,a,b) for(int i = a; i >= (int)(b); i--)
+#define pb push_back
+#define eb emplace_back
+#define pob pop_back
+#define si(a) (int)a.size()
+#define all(a) a.begin(),a.end()
+#define rall(a) a.rbegin(),a.rend()
+#define ret(x) { cout<<(x)<<endl;}
+typedef long long ll;
+using namespace std;
+using P = pair<ll,ll>;
+const ll LINF = 1001002003004005006ll;
+const int INF = 1001001001;
+ll mul(ll a, ll b) { if (a == 0) return 0; if (LINF / a < b) return LINF; return min(LINF, a * b); }
+ll mod(ll x, ll y){return (x % y + y) % y;}
+char itoc(int x){return x + '0';}
+int ctoi(char c){return c - '0';}
+void chmin(ll& x,ll y){x = min(x,y);}
+void chmax(ll& x,ll y){x = max(x,y);}
+ll getnum(ll x, ll y, ll H, ll W) { (void) H; return (x * W + y);}
+template<typename T>
+void print(vector<T> &p){rep(i,si(p)) cout << p[i] << " "; cout << endl;}
+
+bool IsPrime(ll num)//**************************素数判定***************************:
+{
+    if (num < 2) return false;
+    else if (num == 2) return true;
+    else if (num % 2 == 0) return false; //偶数はあらかじめ除く
+
+    double sqrtNum = sqrt(num);
+    for (int i = 3; i <= sqrtNum; i += 2)
+    {
+        if (num % i == 0)
+        {
+            // 素数ではない
+            return false;
+        }
+    }
+
+    // 素数である
+    return true;
+}
+
+
+int main(){
+
+    // ifstream in("???.txt");
+    // cin.rdbuf(in.rdbuf());
+
+    ll num = -1;
+    ll ans = 0;
+
+    rng(a,-1000,1001){
+        rng(b,-1000,1001){
+            rep(n,INF){
+                if(!IsPrime(n*n + a*n + b)){
+                    if(num < n){
+                        num = n;
+                        ans = a*b;
+                    }
+                    break;
+                }
+            }
+
+        }
+    }
+
+    cout << ans << endl;
+
+
+
+    return 0;
+}
